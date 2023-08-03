643dffe52500ae3625db9b661aeb4f65d5a36b3e
P.Y <pedram.yekta12@gmail.com>
2023-08-02 20:41:39 -0400

    Added Dependecy and NavBar

---
+++ source_code/src/Components/NavBar/NavBar.style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    background: #272829;
+    padding: 0 20px;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px 0px;
+    margin: 0 auto;
+`; 
+
+export const Text = styled.div`
+    h3{
+        color: #FFF6E0;
+    }
+`;
---
+++ source_code/src/Components/NavBar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Wrapper, Content, Text} from "./NavBar.style";
+
+const NavBar = () => (
+    <Wrapper>
+        <Content>
+            <Text>
+                <h3>
+                    Hi
+                </h3>
+            </Text>
+        </Content>
+    </Wrapper>
+)
+
+export default NavBar;
